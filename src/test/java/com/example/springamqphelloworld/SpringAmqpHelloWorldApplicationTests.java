a309c4f6e4ea3ea5c13b511e0b211fdd1d08d32f
mucahit bayar <mucahit.b@mindbehind.com>
2024-03-26 11:30:42 +0300

    init

---
+++ src/test/java/com/example/springamqphelloworld/SpringAmqpHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springamqphelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAmqpHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
